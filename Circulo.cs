3679d121fdc2ff6ca86c0d2816feb6d18ac266c9
Mauricio Vergara <mauricioverg@hotmail.es>
2025-04-24 16:21:09 -0500

    first commit

---
+++ Circulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseTaller5
+{
+    internal class Circulo
+    {
+        private float r;
+
+        public Circulo( float r)
+        {
+            this.r = r;
+        }
+
+        public float Area() => 3.14f * r * r;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseTaller5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t1 = new Triangle(0.5f, 0.2f);
+            Triangle t2 = new Triangle();
+        }
+    }
+
+}
